3ffa199e34a79118b9975bbdb2824994010b3ad9
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2022-10-24 16:44:28 -0700

    sign up screen

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from distutils.log import debug
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.gridlayout import GridLayout
 
 Builder.load_file('design.kv')
 
 # create classe that have same name as rules in kivy
 
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
 
 class RootWidget(ScreenManager):
     pass
@@ -16,6 +18,9 @@ class RootWidget(ScreenManager):
 class MainApp(App):
     def build(self):
         return RootWidget()
+
+class SignUpScreen(Screen):
+    pass
 
 
 if __name__ == "__main__":
